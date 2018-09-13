7f3738e2cbb3cced9d130e3af44d2989a23de29e
Kaciras <kaciras@outlook.com>
2018-09-13 13:13:44 +0800

    add staticFile middleware.

--- app.js
+++ app.js
@@ -10,6 +10,28 @@ const config = require("./config");
 const image = require("./image");
 
 
+/**
+ * 能够发送一个位于网站内容目录下的文件。
+ *
+ * @param path 文件路径，是URL中的path部分，以/开头
+ * @return {Function} 中间件函数
+ */
+function staticFile (path) {
+	if (path.startsWith("/static/")) {
+		throw new Error("静态文件目录请用 koa-static 处理");
+	}
+	return function (ctx, next) {
+		if (ctx.path !== path) {
+			return next();
+		}
+		if (ctx.method !== "GET") { // 静态文件仅支持GET
+			ctx.status = 405;
+			return Promise.resolve();
+		}
+		return send(ctx, path, { root: config.content, maxage: 365 * 24 * 3600 * 1000 });
+	};
+}
+
 const app = new Koa();
 const uploader = multer({
 	limits: config.image.maxSize,
@@ -20,12 +42,7 @@ app.use(conditional());
 app.use(uploader.single("file"));
 
 // robots.txt 帮助爬虫抓取，并指向站点地图
-app.use((ctx, next) => {
-	if (ctx.request.path === "/robots.txt") {
-		return send(ctx, "robots.txt");
-	}
-	return next();
-});
+app.use(staticFile("/robots.txt"));
 
 app.use(require("./sitemap"));
 app.use(image); // 图片太大不计算etag，也不需要二次压缩
