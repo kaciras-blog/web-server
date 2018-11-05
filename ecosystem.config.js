ddc0abb99d63e10cf09b276a24595fbda8c00eb7
Kaciras <kaciras@outlook.com>
2018-11-05 16:31:27 +0800

    remove log for app error, use pm2 log system to record console output instead.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,11 @@
 module.exports = {
 	apps: [{
-		name: "Blog-Webserver",
+		name: "blog",
 		script: "index.js",
 
 		instances: 1,
 		autorestart: true,
 		watch: false,
-		max_memory_restart: "100M",
+		max_memory_restart: "64M",
 	}],
 };
