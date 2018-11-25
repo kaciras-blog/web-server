9aab305b0cdf7d3ee599c234ad6d3dff387b7642
Kaciras <kaciras@outlook.com>
2018-11-25 22:04:48 +0800

    fix config.production.js & remove redundant html middleware.

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
 	contentRoot: "D:/Project/Blog/WebContent/dist",
 	imageRoot: "G:/备份/blog.kaciras.net/image",
 
-	cacheMaxAge: 30 * 24 * 60 * 60 * 1000,
+	cacheMaxAge: 12 * 30 * 86400 * 1000,
 
 	cors: {
 		maxAge: 864000,
@@ -16,8 +16,8 @@ module.exports = {
 
 	server: {
 		port: 80,
-		tls: true,
 		httpsPort: 443,
+		tls: true,
 		certificate: "D:/Coding/Utils/dev.pem",
 		privatekey: "D:/Coding/Utils/dev.pvk",
 		redirectHttp: true,
--- config.production.js
+++ config.production.js
@@ -1,9 +1,9 @@
 const config = require("./config");
 
-config.contentRoot = "../content/dist";
+config.contentRoot = "../wwwroot/dist";
 config.imageRoot = "./image";
 
 config.server.certificate = "/etc/letsencrypt/live/blog.kaciras.net/cert.pem";
-config.server.certificate = "/etc/letsencrypt/live/blog.kaciras.net/privkey.pem";
+config.server.privatekey = "/etc/letsencrypt/live/blog.kaciras.net/privkey.pem";
 
 module.exports = config;
