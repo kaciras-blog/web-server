4f9c3d172b6b06fc67f2d546dda6176e46e132a1
Kaciras <kaciras@outlook.com>
2019-01-05 12:21:00 +0800

    refactor cli-server, cli-core

--- packages/cli-core/index.ts
+++ packages/cli-core/index.ts
@@ -1,45 +1,4 @@
-import { getLogger } from "log4js";
 import { Options } from "webpack";
-import build from "../cli-dev/plugins/build";
-import service from "cli-server";
-import log4js, { Configuration } from "log4js";
-
-require("source-map-support").install();
-
-
-/**
- * 配置日志功能，先于其他模块执行保证日志系统的完整。
- */
-function configureLog4js ({ logLevel, logFile }: { logLevel: string, logFile: string | boolean }) {
-	const logConfig: Configuration = {
-		appenders: {
-			console: {
-				type: "stdout",
-				layout: {
-					type: "pattern",
-					pattern: "%[%d{yyyy-MM-dd hh:mm:ss} [%p] %c - %]%m",
-				},
-			},
-		},
-		categories: {
-			default: { appenders: ["console"], level: logLevel },
-		},
-	};
-	if (logFile) {
-		logConfig.appenders.file = {
-			type: "file",
-			filename: logFile,
-			flags: "w",
-			encoding: "utf-8",
-			layout: {
-				type: "pattern",
-				pattern: "%d{yyyy-MM-dd hh:mm:ss} [%p] %c - %m",
-			},
-		};
-		logConfig.categories.default.appenders = ["file"];
-	}
-	log4js.configure(logConfig);
-}
 
 
 /* =========================================================================== *\
@@ -47,18 +6,8 @@ function configureLog4js ({ logLevel, logFile }: { logLevel: string, logFile: st
 \* =========================================================================== */
 
 export interface WebServerConfiguration {
-	server?: ServerOptions;
 	dev?: DevelopmentOptions;
 	webpack?: WebpackOptions;
-}
-
-export interface ServerOptions {
-	port?: number;
-	httpsPort?: number;
-	tls?: boolean;
-	certificate?: string;
-	privatekey?: string;
-	redirectHttp?: boolean;
 }
 
 export interface DevelopmentOptions {
@@ -90,30 +39,12 @@ export interface WebpackOptions {
 	vueLoader?: any;
 }
 
-configureLog4js({ logFile: false, logLevel: "info" });
 
-const optionsFile = process.argv[2];
-if (!optionsFile) {
-	console.error("Configuration not specified");
-	process.exit(1);
+export interface CliConfig {
+	plugins?: any[];
+	service?: CliService[];
 }
 
-// 捕获全局异常记录到日志中。
-const logger = getLogger("system");
-process.on("unhandledRejection", (reason, promise) => logger.error("Unhandled", reason, promise));
-process.on("uncaughtException", (err) => logger.error(err.message, err.stack));
-
-const options = require(optionsFile);
-switch (process.argv[3]) {
-	case "build":
-		build(options.webpack);
-		break;
-	case "serve":
-		service(options, true);
-		break;
-	case "run":
-		service(options, false);
-		break;
-	default:
-		logger.error("Unresloved command: " + process.argv[3]);
+export interface CliService {
+	serve (plugins: any[]): void | Promise<void>;
 }
--- packages/cli-server/index.ts
+++ packages/cli-server/index.ts
@@ -1,13 +1,11 @@
-import { configureWebpack, devMiddleware, prodMiddleware } from "cli-plugin-vue";
-import Koa from "koa";
+import { CliService } from "cli-core";
+import fs from "fs-extra";
+import http, { IncomingMessage, ServerResponse } from "http";
+import http2, { Http2ServerRequest, Http2ServerResponse } from "http2";
+import Koa, { Middleware } from "koa";
 import log4js from "log4js";
-import dev from "../cli-core/plugins/dev";
-import blogPlugin from "../kaciras-blog";
-import { intercept } from "./middleware";
-import createServer from "./server";
 
 const compress = require("koa-compress");
-const serve = require("koa-static");
 const etag = require("koa-etag");
 const conditional = require("koa-conditional-get");
 const cors = require("@koa/cors");
@@ -20,46 +18,162 @@ const logger = log4js.getLogger("app");
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 
-function setupBasicMiddlewares (app: Koa, options: any) {
-	app.use(cors(options.blog.cors));
-	app.use(conditional());
+// function setupBasicMiddlewares (app: Koa, options: any) {
+// 	app.use(cors(options.blog.cors));
+// 	app.use(conditional());
+//
+// 	const uploader = multer({ limits: 16 * 1024 * 1024 });
+// 	app.use(uploader.single("file"));
+//
+// 	app.use(compress({ threshold: 2048 }));
+// 	blogPlugin(app, options.blog); // 图片太大不计算etag
+//
+// 	app.use(etag());
+// 	app.use(intercept([
+// 		"/index.template.html",
+// 		"/vue-ssr-client-manifest.json",
+// 		"/vue-ssr-server-bundle.json",
+// 	]));
+//
+// 	app.use(serve(options.webpack.outputPath, {
+// 		index: false,
+// 		maxage: 30 * 86400 * 1000,
+// 	}));
+// }
+//
+// async function u (options: any, devserver: boolean /* 临时 */) {
+// 	const app = new Koa();
+//
+// 	if (devserver) {
+// 		const clientConfig = require("../cli-dev/template/client.config").default(options.webpack);
+// 		configureWebpack(clientConfig);
+// 		const middleware = await dev(options, clientConfig);
+//
+// 		app.use(middleware); // 这个得放在koa-compress前头。
+// 		setupBasicMiddlewares(app, options);
+// 		app.use(await devMiddleware(options));
+// 	} else {
+// 		logger.info("No webpack config specified, run as production mode.");
+//
+// 		setupBasicMiddlewares(app, options);
+// 		app.use(await prodMiddleware(options));
+// 	}
+// }
 
-	const uploader = multer({ limits: 16 * 1024 * 1024 });
-	app.use(uploader.single("file"));
-
-	app.use(compress({ threshold: 2048 }));
-	blogPlugin(app, options.blog); // 图片太大不计算etag
-
-	app.use(etag());
-	app.use(intercept([
-		"/index.template.html",
-		"/vue-ssr-client-manifest.json",
-		"/vue-ssr-server-bundle.json",
-	]));
-
-	app.use(serve(options.webpack.outputPath, {
-		index: false,
-		maxage: 30 * 86400 * 1000,
-	}));
+export interface CliServerPligun {
+	configureCliServer (api: CliServerAPI): void;
 }
 
-export default async function (options: any, devserver: boolean /* 临时 */) {
-	const app = new Koa();
+class CliServerAPI {
 
-	if (devserver) {
-		const clientConfig = require("../cli-core/template/client.config").default(options.webpack);
-		configureWebpack(clientConfig);
-		const middleware = await dev(options, clientConfig);
+	private readonly beforeAll: Middleware[] = [];
+	private readonly beforeFilter: Middleware[] = [];
+	private readonly filter: Middleware[] = [];
+	private readonly resource: Middleware[] = [];
 
-		app.use(middleware); // 这个得放在koa-compress前头。
-		setupBasicMiddlewares(app, options);
-		app.use(await devMiddleware(options));
-	} else {
-		logger.info("No webpack config specified, run as production mode.");
+	private fallBack?: Middleware;
 
-		setupBasicMiddlewares(app, options);
-		app.use(await prodMiddleware(options));
+	useFallBack (middleware: Middleware) {
+		if (this.fallBack) {
+			throw new Error("A fall back middleware already exists.");
+		}
+		this.fallBack = middleware;
 	}
 
-	await createServer(app.callback(), options.server);
+	configure (application: Koa) {
+		const setup = application.use.bind(application);
+
+		this.beforeAll.forEach(setup);
+		this.beforeFilter.forEach(setup);
+		this.filter.forEach(setup);
+		this.resource.forEach(setup);
+
+		if (this.fallBack) {
+			application.use(this.fallBack);
+		}
+		return application;
+	}
+
+	useBeforeAll (middleware: Middleware): this {
+		this.beforeAll.push(middleware);
+		return this;
+	}
+
+	useBeforeFilter (middleware: Middleware): this {
+		this.beforeFilter.push(middleware);
+		return this;
+	}
+
+	useFilter (middleware: Middleware): this {
+		this.filter.push(middleware);
+		return this;
+	}
+
+	useResource (middleware: Middleware): this {
+		this.resource.push(middleware);
+		return this;
+	}
+}
+
+export interface CliServerOptions {
+	port?: number;
+	httpsPort?: number;
+	tls?: boolean;
+	certificate?: string;
+	privatekey?: string;
+	redirectHttp?: boolean;
+}
+
+// app.callback() 的定义，比较长不方便直接写在参数里
+type OnRequestHandler = (req: IncomingMessage | Http2ServerRequest, res: ServerResponse | Http2ServerResponse) => void;
+
+export default class CliHttpService implements CliService {
+
+	private readonly options: CliServerOptions;
+
+	constructor (options: CliServerOptions) {
+		this.options = options;
+	}
+
+	serve (plugins: any[]): void | Promise<void> {
+		const api = new CliServerAPI();
+		api.useBeforeAll(conditional());
+		api.useBeforeAll(multer({ limits: 16 * 1024 * 1024 }).single("file"));
+		api.useFilter(etag());
+		api.useFilter(compress({ threshold: 2048 }));
+
+		plugins.filter((p) => p.CliServerPligun).forEach((p) => p.CliServerPligun(api));
+
+		const app = new Koa();
+		api.configure(app);
+		this.createServer(app.callback());
+	}
+
+	createServer (requestHandler: OnRequestHandler) {
+		const {
+			port = 80, httpsPort = 443,
+			tls, privatekey, certificate, redirectHttp,
+		} = this.options;
+
+		if (tls) {
+			if (!privatekey || !certificate) {
+				throw new Error("You must specifiy privatekey and certificate with tls enabled.");
+			}
+			http2.createSecureServer({
+				key: fs.readFileSync(privatekey),
+				cert: fs.readFileSync(certificate),
+				allowHTTP1: true,
+			}, requestHandler)
+				.listen(httpsPort, () => logger.info(`Https连接端口：${httpsPort}`));
+		}
+
+		if (redirectHttp) {
+			http.createServer((req, res) => {
+				res.writeHead(301, { Location: "https://" + req.headers.host + req.url });
+				res.end();
+			}).listen(port, () => logger.info(`重定向来自端口：${port}的请求至：${httpsPort}`));
+		} else {
+			http.createServer(requestHandler).listen(port, () => logger.info(`在端口：${port}上监听Http连接`));
+		}
+	}
 }
