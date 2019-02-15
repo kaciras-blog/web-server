c13f664c6fd7d828a1d4ec274b3e290af841921b
Kaciras <kaciras@outlook.com>
2019-02-15 17:50:23 +0800

    enhance app.runServer(): * make it returns a promise * return a function allows to close created servers

--- packages/kxc-server/__tests__/create-http-server.ts
+++ packages/kxc-server/__tests__/create-http-server.ts
@@ -3,17 +3,30 @@ import http from "http";
 
 const HTTP_URL = "http://localhost/";
 const HTTPS_URL = "https://localhost/";
-const REQUEST_HANDLER = () => {};
+
+const OPTIONS = {
+	tls: true,
+	certificate: "D:/Coding/Utils/dev.pem",
+	privatekey: "D:/Coding/Utils/dev.pvk",
+	redirectHttp: true,
+};
+
+let closeFunction: () => void;
+
+beforeAll(async () => {
+	closeFunction = await runServer((req, res: any) => res.end("hellow"), OPTIONS);
+});
+afterAll(() => closeFunction());
+
 
 describe("app.runServer", () => {
 
-	it("should redirect to https", (done) => {
-		runServer(REQUEST_HANDLER, { redirectHttp: true });
+	it("should redirect to https", async (done) => {
 
 		http.get(HTTP_URL, ((res) => {
 			expect(res.statusCode).toEqual(301);
 			expect(res.headers.location).toEqual(HTTPS_URL);
 			done();
-		}));
+		})).end();
 	});
 });
--- packages/kxc-server/app.ts
+++ packages/kxc-server/app.ts
@@ -1,6 +1,8 @@
 import fs from "fs-extra";
 import http, { IncomingMessage, ServerResponse } from "http";
 import http2, { Http2ServerRequest, Http2ServerResponse } from "http2";
+import { Server } from "net";
+import { promisify } from "util";
 import log4js from "log4js";
 
 
@@ -19,8 +21,14 @@ export interface ServerOptions {
 // app.callback() 的定义，比较长不方便直接写在参数里
 type OnRequestHandler = (req: IncomingMessage | Http2ServerRequest, res: ServerResponse | Http2ServerResponse) => void;
 
-export function runServer (requestHandler: OnRequestHandler, options: ServerOptions) {
+/** 将 Server.listen 转成异步方法并调用，返回server */
+function listenAsync (server: Server, port: number) {
+	return promisify(server.listen.bind(server))(port).then(() => server);
+}
+
+export async function runServer (requestHandler: OnRequestHandler, options: ServerOptions) {
 	const { port = 80, httpsPort = 443 } = options;
+	const servers: Server[] = [];
 
 	if (options.tls) {
 		const { privatekey, certificate } = options;
@@ -28,25 +36,30 @@ export function runServer (requestHandler: OnRequestHandler, options: ServerOpti
 		if (!privatekey || !certificate) {
 			throw new Error("You must specifiy privatekey and certificate with tls enabled.");
 		}
-		http2.createSecureServer({
-			key: fs.readFileSync(privatekey),
-			cert: fs.readFileSync(certificate),
+		const server = http2.createSecureServer({
 			allowHTTP1: true,
-		}, requestHandler).listen(httpsPort, () => {
-			logger.info(`Https连接端口：${httpsPort}`);
-		});
+			cert: fs.readFileSync(certificate),
+			key: fs.readFileSync(privatekey),
+		}, requestHandler);
+
+		servers.push(await listenAsync(server, httpsPort));
+		logger.info(`Https连接端口：${httpsPort}`);
 	}
 
 	if (options.redirectHttp) {
+		const portPart = httpsPort === 443 ? "" : ":" + httpsPort;
 		// TODO: Remove type fix
-		http.createServer((req, res: any) => {
-			res.writeHead(301, { Location: `https://${req.headers.host}${req.url}` }).end();
-		}).listen(port, () => {
-			logger.info(`重定向来自端口：${port}的请求至：${httpsPort}`);
+		const server = http.createServer((req, res: any) => {
+			res.writeHead(301, { Location: `https://${req.headers.host}${portPart}${req.url}` }).end();
 		});
+		servers.push(await listenAsync(server, port));
+		logger.info(`重定向来自端口：${port}的请求至：${httpsPort}`);
 	} else {
-		http.createServer(requestHandler).listen(port, () => {
-			logger.info(`在端口：${port}上监听Http连接`);
-		});
+		const server = http.createServer(requestHandler);
+		servers.push(await listenAsync(server, port));
+		logger.info(`在端口：${port}上监听Http连接`);
 	}
+
+	// Keep-Alive 的连接无法关闭，反而会使close方法一直等待，所以close的参数里没有回调
+	return () => servers.forEach((s) => s.close());
 }
