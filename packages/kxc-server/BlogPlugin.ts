ed4f0c8b648c05687ff2edce90fbce4df6a75e6d
Kaciras <Kaciras@outlook.com>
2019-06-03 17:55:11 +0800

    add CSP report support

--- packages/kxc-server/BlogPlugin.ts
+++ packages/kxc-server/BlogPlugin.ts
@@ -8,6 +8,10 @@ import { intercept, serviceWorkerToggle } from "./infra/middlewares";
 import { createSitemapMiddleware } from "./sitemap";
 import multer = require("koa-multer");
 import { ParameterizedContext } from "koa";
+import log4js from "log4js";
+import bodyParser from "koa-bodyparser";
+
+const logger = log4js.getLogger();
 
 
 export interface AppOptions extends ImageMiddlewareOptions {
@@ -16,10 +20,23 @@ export interface AppOptions extends ImageMiddlewareOptions {
 	serverCert: string;
 }
 
-export async function AdvancedSecurityFilter(ctx: ParameterizedContext, next: () => Promise<any>) {
+async function AdvancedSecurityFilter(ctx: ParameterizedContext, next: () => Promise<any>) {
 	await next();
 	ctx.set("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload");
-	ctx.set("Content-Security-Policy", "frame-ancestors 'self'; block-all-mixed-content");
+	ctx.set("Content-Security-Policy", "frame-ancestors 'self'; " +
+		"object-src 'none'; block-all-mixed-content; report-uri /csp-report");
+}
+
+function CSRReportListener(ctx: ParameterizedContext, next: () => Promise<any>) {
+	if (ctx.path !== "/csp-report") {
+		return next();
+	}
+	if (ctx.request.body) {
+		logger.warn("CSP Violation: ", ctx.request.body);
+	} else {
+		logger.warn("CSP Violation: No data received!");
+	}
+	ctx.status = 204;
 }
 
 export default class BlogPlugin implements ClassCliServerPligun {
@@ -36,12 +53,14 @@ export default class BlogPlugin implements ClassCliServerPligun {
 		api.useBeforeAll(AdvancedSecurityFilter);
 		api.useBeforeAll(conditional());
 		api.useBeforeAll(cors(options.cors));
+		api.useBeforeAll(bodyParser());
 
 		const uploader = multer({ limits: { fileSize: 4 * 1024 * 1024 } });
 		api.useBeforeAll(uploader.single("file"));
 
 		api.useBeforeFilter(serviceWorkerToggle(true));
 		api.useBeforeFilter(createImageMiddleware(options)); // 图片太大不计算etag
+		api.useBeforeFilter(CSRReportListener);
 
 		api.useFilter(intercept(/\.(?:js|css)\.map$/));
 		api.useFilter(compress({ threshold: 1024 }));
