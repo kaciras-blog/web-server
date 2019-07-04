fbe200afc32e5bf7d0a41f2536a1949f7a6c018a
Kaciras <Kaciras@outlook.com>
2019-07-04 17:03:55 +0800

    temp commit

--- packages/kxc-server/image-converter.ts
+++ packages/kxc-server/image-converter.ts
@@ -1,65 +1,95 @@
 /**
- * imagemin 内部将图片缓冲写入到临时文件，然后启动进程执行相关的图片处理程序，这种方法很低效。
- * 而sharp则直接调用相关的图片处理库更好些，但是目前sharp还不是很完善？以后考虑转过去。
+ * imagemin 对某些格式会将图片缓冲写入到临时文件，然后启动进程执行相关的图片处理程序，这方法很低效。
  */
-import sharp from "sharp";
-import imagemin from "imagemin";
-// import gifsicle from "imagemin-gifsicle";
-// import mozjpeg from "imagemin-mozjpeg";
-// import svgo from "imagemin-svgo";
-// import pngquant from "imagemin-pngquant";
-// import optipng from "imagemin-optipng";
+import sharp, { Sharp } from "sharp";
+import PngQuant from "imagemin-pngquant";
 import SVGO from "svgo";
 import fs from "fs-extra";
-import crypto from "crypto";
 import path from "path";
 
-// libvips 暂不支持动画GIF，8.8版还没正式出
-// libimagequant 是 GPL 协议的，LGPL 的 libvips 默认不包含，非得自己编译不可
-// const image = sharp("C:\\Users\\XuFan\\Desktop\\jaanus-jagomagi-1245520-unsplash.jpg");
-// image.png({ palette: false })
-// 	.toFile("C:\\Users\\XuFan\\Desktop\\test2.png")
-// 	.catch((err) => console.log(err));
+export interface ResizeOptions {
+	readonly width?: number;
+	readonly height?: number;
+	readonly force?: boolean;
+}
 
-// const plugins = [
-// 	gifsicle(),
-// 	mozjpeg(),
-// 	svgo(),
-// 	pngquant(),
-// 	optipng(),
-// ];
+export interface ImageEntity {
+	hash: string;
+	type: string;
+	buffer: Buffer;
+}
 
 const svgOptimizer = new SVGO();
-const imageRoot = "C:\\Users\\XuFan\\Desktop";
+const pngquant = PngQuant();
 
-export async function compress(buffer: Buffer, originType: string) {
-	const hash = crypto
-		.createHash("sha3-256")
-		.update(buffer)
-		.digest("hex");
+async function applyResize(image: Sharp, options: ResizeOptions) {
+	const { width, height, force } = options;
+	if (force) {
+		return image.resize(width, height, { fit: "outside" });
+	}
+	const source = await image.metadata();
+	if (width && source.width! > width) {
+		return image.resize(width, null, { fit: "outside" });
+	}
+	if (height && source.height! > height) {
+		return image.resize(null, height, { fit: "outside" });
+	}
+	return image;
+}
 
-	if (originType === "svg") {
-		const optimized = await svgOptimizer.optimize(buffer.toString());
-		return await fs.writeFile(path.join(imageRoot, `${hash}.${originType}`), optimized.data);
+export class LocalImageStore {
+
+	private readonly directory: string;
+
+	constructor(directory: string) {
+		this.directory = directory;
 	}
 
-	let image = sharp(buffer);
-	if (originType !== "webp") {
-		await image.webp().toFile(path.join(imageRoot, `${hash}.webp`));
+	async save(input: ImageEntity) {
+		const { hash, buffer, type } = input;
+
+		// 矢量图没有压缩和缩放，单独处理
+		if (type === "svg") {
+			const optimized = await svgOptimizer.optimize(buffer.toString());
+			return await fs.writeFile(this.originPath(hash, type), optimized.data);
+		}
+
+		let image = sharp(buffer);
+
+		// 保存原图，BMP 图片保存为无损 PNG
+		if (type === "bmp") {
+			await image.png().toFile(this.originPath(hash, "png"));
+		} else {
+			await fs.writeFile(this.originPath(hash, type), buffer);
+		}
+
+		// 保存转换后的图片
+		if (type !== "webp") {
+			await image.webp().toFile(this.cachePath(hash, "origin", "webp"));
+		}
+
+		switch (input.type) {
+			case "jpg":
+			case "bmp":
+				image = image.jpeg();
+				break;
+			case "png":
+				image = await image.png();
+				// pngquant
+				break;
+		}
+		// TODO
 	}
 
-	switch (originType) {
-		case "jpg":
-		case "bmp":
-			image = image.jpeg();
-			originType = "jpg";
-			break;
-		case "png":
-			image = image.png();
-			break;
+	select(hash: string): Buffer {
+		throw new Error();
 	}
 
-	return image.toFile(path.join(imageRoot, `${hash}.${originType}`));
+	private cachePath(hash: string, scale: string, type: string) {
+		return path.join(this.directory, scale, type, `${hash}.${type}`);
+	}
 
-	// return imagemin.buffer(buffer, { plugins });
+	private originPath(hash: string, type: string) {
+		return path.join(this.directory, "origin", `${hash}.${type}`);
+	}
 }
