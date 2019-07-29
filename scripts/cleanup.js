68e3f5c4a1a3410c8e23e2f09c4f28ad8a715a4c
Kaciras <Kaciras@outlook.com>
2019-07-29 23:04:49 +0800

    fix cleanup script

--- scripts/cleanup.js
+++ scripts/cleanup.js
@@ -1,15 +1,21 @@
 const fs = require("fs-extra");
 const chalk = require("chalk");
-const globby = require("globby") ;
+const globby = require("globby");
+const path = require("path");
 
-
-async function cleanup () {
-	const files = await globby(["packages/**/*.{js,map,d.ts}", "!packages/**/__mocks__"]);
+/** 删除所有自动生成的文件 */
+async function cleanup() {
+	const files = await globby([
+		"packages/**/*.{js,map,d.ts}",
+		"!**/node_modules/**",
+		"!packages/**/__mocks__",
+	]);
 	for (const file of files) {
-		console.log(`Remove: ${file}`);
 		await fs.remove(file);
+		console.log(`Remove ${file}`);
 	}
 	console.log(chalk.blue("\nCleanup complete."));
 }
 
+process.chdir(path.dirname(__dirname));
 cleanup().catch((err) => console.error(err));
