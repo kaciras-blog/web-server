77825868e71e8861afee66793e796127b1f3d630
Kaciras <Kaciras@outlook.com>
2019-11-08 19:17:35 +0800

    remove unnecessary `transform` in jest config.

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,3 @@
-const path = require("path");
-
 module.exports = {
 	clearMocks: true, // Automatically clear mock calls and instances between every test
 	coverageDirectory: "coverage", // The directory where Jest should output its coverage files
@@ -9,10 +7,7 @@ module.exports = {
 	testEnvironment: "node",
 
 	testMatch: [
-		"**/__tests__/**/*-tests.+(ts|tsx)",
 		"**/?(*.)+(spec|test).+(ts|tsx)",
+		"**/__tests__/**/*-tests.+(ts|tsx)",
 	],
-	transform: {
-		"^.+\\.(ts|tsx)$": "ts-jest",
-	},
 };
