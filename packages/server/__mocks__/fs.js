24eff8fd9ca7b1146ec702d94f4c0ca834764a6a
Kaciras <Kaciras@outlook.com>
2019-11-09 18:36:19 +0800

    ensure temp directory exists on fs mock.

--- packages/server/__mocks__/fs.js
+++ packages/server/__mocks__/fs.js
@@ -1,2 +1,8 @@
+const os = require("os");
 const { fs } = require("memfs");
+
 module.exports = fs;
+
+// 解决临时目录不存在的问题。因为通常都会假定系统的临时目录是存在的，但memfs并不自动创建这个目录，
+// 这就会造成文件夹不存在问题。
+fs.mkdirSync(os.tmpdir(), { recursive: true });
