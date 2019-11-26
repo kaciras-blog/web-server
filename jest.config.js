f964dff600c4b924a6f4baade3d7757e4bcd6449
Kaciras <Kaciras@outlook.com>
2019-11-26 22:17:59 +0800

    jest: fix moduleFileExtensions

--- jest.config.js
+++ jest.config.js
@@ -7,12 +7,16 @@ module.exports = {
 		"!packages/*/lib/**/*.d.ts",
 	],
 
-	moduleFileExtensions: ["js", "json", "ts", "mjs"],
+	// 可倒入文件的扩展名，在前面的将优先匹配。
+	// 使用 TypeScript 时必须把 ts 放在 js 前面，否则它会使用生成的 js 文件而不是源文件。
+	// https://jestjs.io/docs/en/configuration#modulefileextensions-arraystring
+	moduleFileExtensions: ["ts", "js", "mjs", "json"],
+
 	preset: "ts-jest",
 	testEnvironment: "node",
 
 	testMatch: [
-		"**/?(*.)+(spec|test).+(ts|tsx)",
-		"**/__tests__/**/*-tests.+(ts|tsx)",
+		"**/?(*.)+(spec|test).+ts",
+		"**/__tests__/**/*-tests.+ts",
 	],
 };
