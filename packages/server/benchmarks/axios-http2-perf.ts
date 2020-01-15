5369b20ca298e7a1325f79de7bd47c4f57eeb2c5
Kaciras <Kaciras@outlook.com>
2020-01-15 21:34:23 +0800

    refactor axios-http2-perf.ts

--- packages/server/benchmarks/axios-http2-perf.ts
+++ packages/server/benchmarks/axios-http2-perf.ts
@@ -1,43 +1,43 @@
+import assert from "assert";
+import { AddressInfo } from "net";
 import http2 from "http2";
 import Axios from "axios";
 import { configureAxiosHttp2 } from "../lib/axios-helper";
-import { AddressInfo } from "net";
 
 /*
- * 测试 axios-http2 的性能：
- * 不缓存 ClientHttp2Session - 1368 毫秒 / 1000次请求
- * 缓存 ClientHttp2Session - 450 毫秒 / 1000次请求
+ * 测试 axios-http2 的性能（1000次请求）：
+ * 缓存 ClientHttp2Session - 481 毫秒
+ * 不缓存 ClientHttp2Session - 1368 毫秒
  */
 const axios = Axios.create();
 configureAxiosHttp2(axios);
+
 const server = http2.createServer(((request, response) => response.end("benchmark")));
 
-async function iterate() {
-	const url = "http://localhost:" + (server.address() as AddressInfo).port;
-	const from = process.hrtime();
-	for (let i = 0; i < 1000; i++) {
-		await axios.get(url);
-		// const res = await axios.get(url);
-		// assert res.status === 200
-		// assert res.data === "benchmark"
-	}
-	const to = process.hrtime();
-	return (to[0] - from[0]) * 1000 + (to[1] - from[1]) / 1000000;
-}
-
-/** 除了测试性能之外，还测试了ClientHttp2Session的超时时间 */
 async function run() {
-	let timeUsage = 0;
-	for (let i = 0; i < 5; i++) {
-		timeUsage += await iterate();
-	}
-	console.log(`平均用时：${timeUsage / 5}ms`);
+	const url = "http://localhost:" + (server.address() as AddressInfo).port;
 
-	const perfEnd = process.hrtime()[0];
-	server.close(() => {
-		const idle = process.hrtime()[0] - perfEnd;
-		console.log(`ClientHttp2Session 在${idle}秒后自动关闭`);
-	});
+	// 先检查一下下服务器是否能正确响应
+	const res = await axios.get(url);
+	assert.equal(res.status, 200);
+	assert.equal(res.data, "benchmark");
+
+	async function iterate() {
+		const from = process.hrtime();
+		for (let i = 0; i < 1000; i++) {
+			await axios.get(url);
+		}
+		const to = process.hrtime();
+		return (to[0] - from[0]) * 1000 + (to[1] - from[1]) / 1000000;
+	}
+
+	let results = 0;
+	for (let i = 0; i < 5; i++) {
+		results += await iterate();
+	}
+
+	server.close();
+	console.log(`平均用时：${results / 5}ms`);
 }
 
 server.listen(0, run);
