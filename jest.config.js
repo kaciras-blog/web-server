2e29aba696e6955f86efd54c6827cd4d8eda0042
Kaciras <Kaciras@outlook.com>
2020-02-04 16:10:52 +0800

    update deps

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,7 @@ module.exports = {
 		"!packages/*/lib/**/*.d.ts",
 	],
 
-	// 可倒入文件的扩展名，在前面的将优先匹配。
+	// 可导入文件的扩展名，在前面的将优先匹配。
 	// 使用 TypeScript 时必须把 ts 放在 js 前面，否则它会使用生成的 js 文件而不是源文件。
 	// https://jestjs.io/docs/en/configuration#modulefileextensions-arraystring
 	moduleFileExtensions: ["ts", "js", "mjs", "json"],
@@ -16,7 +16,7 @@ module.exports = {
 	testEnvironment: "node",
 
 	testMatch: [
-		"**/?(*.)+(spec|test).+ts",
-		"**/__tests__/**/*-tests.+ts",
+		"**/?(*.)+(spec|test).ts",
+		"**/__tests__/**/*-tests.ts",
 	],
 };
