bf668533dbbbba60dc46b4d1727a954e403a039b
Kaciras <Kaciras@outlook.com>
2020-05-05 19:24:06 +0800

    fix tests

--- packages/image/__tests__/build-image-cache.spec.ts
+++ packages/image/__tests__/build-image-cache.spec.ts
@@ -3,31 +3,35 @@ import path from "path";
 import fs from "fs-extra";
 import { buildCache } from "../lib/build-image-cache";
 
-const tmpdir = fs.mkdtempSync(path.join(os.tmpdir(), "test-"));
+const ROOT = fs.mkdtempSync(path.join(os.tmpdir(), "test-"));
+const NAME = "742c5c7e26d80750f1c32f0bbcf0caab4e41678d03f5d610c3a1057b274b2268.png";
 
-beforeEach(() => fs.emptyDir(tmpdir));
+beforeEach(() => {
+	fs.emptyDirSync(ROOT);
+	fs.ensureDirSync(path.join(ROOT, "image"));
+});
 
 it('should generate caches', async () => {
 	const picture = fs.readFileSync(path.join(__dirname, "fixtures/color_text_black_bg.png"));
-	fs.writeFileSync(path.join(tmpdir, "742c5c7e26d80750f1c32f0bbcf0caab4e41678d03f5d610c3a1057b274b2268.png"), picture);
+	fs.writeFileSync(path.join(ROOT, "image", NAME), picture);
 
-	await buildCache(tmpdir);
+	await buildCache(ROOT);
 
-	expect(fs.readdirSync(path.join(tmpdir, "cache/png"))).toHaveLength(1);
+	expect(fs.readdirSync(path.join(ROOT, "cache/png"))).toHaveLength(1);
 });
 
 it('should skip existing cache files', async () => {
 	const name = "742c5c7e26d80750f1c32f0bbcf0caab4e41678d03f5d610c3a1057b274b2268.png";
-	const cacheDir = path.join(tmpdir, "cache/png/");
+	const cacheDir = path.join(ROOT, "cache/png/");
 	const cacheFile = path.join(cacheDir, name);
 
 	const picture = fs.readFileSync(path.join(__dirname, "fixtures/color_text_black_bg.png"));
-	fs.writeFileSync(path.join(tmpdir, name), picture);
+	fs.writeFileSync(path.join(ROOT, "image", name), picture);
 
 	fs.ensureDirSync(cacheDir);
 	fs.writeFileSync(cacheFile, "");
 
-	await buildCache(tmpdir);
+	await buildCache(ROOT);
 
 	expect(fs.readFileSync(cacheFile)).toHaveLength(0);
 });
