3e3222b1a9e314273642f6c0f2244a0472567916
Kaciras <Kaciras@outlook.com>
2020-05-08 20:44:05 +0800

    set MarkdownIt option `html` to true

--- packages/server/lib/markdown.ts
+++ packages/server/lib/markdown.ts
@@ -1,8 +1,9 @@
+// 这里导出的仅供 Feed 使用
 import MarkdownIt from "markdown-it";
 import katex from "@iktakahiro/markdown-it-katex";
 import tableOfContent from "markdown-it-toc-done-right";
 
-export const markdown = new MarkdownIt();
+export const markdown = new MarkdownIt({ html: true });
 
 markdown.use(katex);
 markdown.use(tableOfContent);
