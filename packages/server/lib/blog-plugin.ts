3d2d25d3e075cfb631cd7d1015edda4b728a8dd5
Kaciras <Kaciras@outlook.com>
2020-05-10 17:29:31 +0800

    (WIP) add video middleware

--- packages/server/lib/blog-plugin.ts
+++ packages/server/lib/blog-plugin.ts
@@ -1,5 +1,8 @@
+import crypto from "crypto";
+import fs from "fs-extra";
+import path from "path";
 import Axios from "axios";
-import { Context, Middleware } from "koa";
+import { Context, Middleware, Next } from "koa";
 import { getLogger } from "log4js";
 import conditional from "koa-conditional-get";
 import cors from "@koa/cors";
@@ -16,6 +19,8 @@ import { createSitemapMiddleware } from "./sitemap";
 import { feedMiddleware } from "./feed";
 import ApplicationBuilder, { FunctionPlugin } from "./ApplicationBuilder";
 import { configureForProxy } from "./axios-helper";
+import mime from "mime-types";
+import sendFileRange from "./send-range";
 
 
 const logger = getLogger();
@@ -70,15 +75,12 @@ function createImageMiddleware(options: AppOptions) {
 	let uploadFn: Middleware = (ctx) => uploadImage(service, ctx);
 
 	// 限制上传用户，仅博主能上传
-	const checkPermission: Middleware = async (ctx, next) => {
+	async function onlyAdministrator(ctx: Context, next: Next) {
 		const { data } = await Axios.get(url, configureForProxy(ctx));
-		if (data.id === 2) {
-			return next();
-		}
-		ctx.status = 403;
-		ctx.body = "你没有权限上传图片";
-	};
-	uploadFn = compose<Context>([checkPermission, uploadFn]);
+		return data.id === 2 ? next() : (ctx.status = 403);
+	}
+
+	uploadFn = compose<Context>([onlyAdministrator, uploadFn]);
 
 	return route("/image", downloadFn, uploadFn);
 }
@@ -86,9 +88,43 @@ function createImageMiddleware(options: AppOptions) {
 // 【注意】没有使用 Etag，因为所有资源都可以用时间缓存，而且 koa-etag 内部使用 sha1 计算 Etag，
 // 对于图片这样较大的资源会占用 CPU，而我的VPS处理器又很垃圾。
 export default function getBlogPlugin(options: AppOptions): FunctionPlugin {
+	const videoDir = path.join(options.dataDir, "video");
+	fs.ensureDirSync(videoDir);
+	const url = options.serverAddress + "/session/user";
+
+	async function videoMiddleware(ctx: Context, next: Next) {
+		if (!ctx.path.startsWith("/video")) {
+			return next();
+		}
+		if (ctx.method === "GET") {
+			const name = path.basename(ctx.path.substring(6));
+			const fullname = path.join(videoDir, name);
+			const stats = await fs.stat(fullname);
+			return sendFileRange(ctx, fullname, stats.size);
+		}
+		if (ctx.method === "POST") {
+			const { data } = await Axios.get(url, configureForProxy(ctx));
+			if (data.id !== 2) {
+				return ctx.status = 403;
+			}
+
+			const { buffer } = ctx.file;
+			const hash = crypto
+				.createHash("sha3-256")
+				.update(buffer)
+				.digest("hex");
+
+			const name = hash + "." + mime.extension(ctx.file.mimetype);
+			await fs.writeFile(path.join(videoDir, name), buffer);
+
+			ctx.status = 201;
+			ctx.set("Location", `${ctx.path}/${name}`);
+		} else {
+			ctx.status = 415;
+		}
+	}
 
 	return (api: ApplicationBuilder) => {
-
 		api.useBeforeAll(cors({
 			origin: (ctx) => ctx.protocol + options.host,
 			credentials: true,
@@ -100,7 +136,7 @@ export default function getBlogPlugin(options: AppOptions): FunctionPlugin {
 		api.useBeforeAll(conditional());
 		api.useBeforeAll(bodyParser());
 
-		const uploader = multer({ limits: { fileSize: 4 * 1024 * 1024 } });
+		const uploader = multer({ limits: { fileSize: 10 * 1024 * 1024 } });
 		api.useBeforeAll(uploader.single("file"));
 
 		installCSPPlugin(api);
@@ -111,6 +147,7 @@ export default function getBlogPlugin(options: AppOptions): FunctionPlugin {
 		api.useFilter(compress({ br: false, threshold: 1024, }));
 
 		api.useResource(createImageMiddleware(options));
+		api.useResource(videoMiddleware);
 		api.useResource(serviceWorkerToggle(options.serviceWorker));
 		api.useResource(createSitemapMiddleware(options.serverAddress));
 		api.useResource(feedMiddleware(options.serverAddress));
