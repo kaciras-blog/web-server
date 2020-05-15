8f120e80002ff2ae280f5597ccb5e5b2974310b3
Kaciras <Kaciras@outlook.com>
2020-05-15 16:45:12 +0800

    fix video-middleware.ts

--- packages/server/lib/video-middleware.ts
+++ packages/server/lib/video-middleware.ts
@@ -5,26 +5,36 @@ import fs from "fs-extra";
 import mime from "mime-types";
 import sendFileRange from "./send-range";
 
-interface VideoDownloadContext extends ExtendableContext{
+interface VideoDownloadContext extends ExtendableContext {
 	params: { name: string }
 }
 
 export async function downloadVideo(directory: string, ctx: VideoDownloadContext) {
 	const name = path.basename(ctx.params.name);
 	const fullname = path.join(directory, name);
-	const stats = await fs.stat(fullname);
-	return sendFileRange(ctx, fullname, stats.size);
+
+	try {
+		const stats = await fs.stat(fullname);
+		return sendFileRange(ctx, fullname, stats.size);
+	} catch (e) {
+		if (e.code !== "ENOENT") throw e;
+	}
 }
 
-export async function uploadVideo(directory: string,ctx: Context) {
+export async function uploadVideo(directory: string, ctx: Context) {
 	const { buffer, mimetype } = ctx.file;
+
 	const hash = crypto
 		.createHash("sha3-256")
 		.update(buffer)
 		.digest("hex");
 
 	const name = hash + "." + mime.extension(mimetype);
-	await fs.writeFile(path.join(directory, name), buffer);
+	try {
+		await fs.writeFile(path.join(directory, name), buffer, { flag: "wx" });
+	} catch (e) {
+		if (e.code !== "EEXIST") throw e;
+	}
 
 	ctx.status = 201;
 	ctx.set("Location", `${ctx.path}/${name}`);
