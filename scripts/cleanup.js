b4e3f033750bbb6c3313e8daacd74405f573b7b8
Kaciras <Kaciras@outlook.com>
2020-06-21 21:14:33 +0800

    fix eslint

--- scripts/cleanup.js
+++ scripts/cleanup.js
@@ -1,7 +1,7 @@
+const { dirname } = require("path");
 const fs = require("fs-extra");
 const chalk = require("chalk");
 const globby = require("globby");
-const path = require("path");
 
 /** 删除所有自动生成的文件 */
 async function cleanup() {
@@ -10,14 +10,16 @@ async function cleanup() {
 		"!packages/*/bin/*.js",
 		"!**/node_modules/**",
 		"!**/__mocks__",
-		"!**/__tests__/fixtures"
+		"!**/__tests__/fixtures",
 	]);
+
 	for (const file of files) {
 		await fs.remove(file);
 		console.log(`Remove ${file}`);
 	}
+
 	console.log(chalk.blue("\nCleanup complete."));
 }
 
-process.chdir(path.dirname(__dirname));
+process.chdir(dirname(__dirname));
 cleanup().catch((err) => console.error(err));
