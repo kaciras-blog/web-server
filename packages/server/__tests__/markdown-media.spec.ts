b2ed7421775402663c8f02f8dcbe806dcea6e23a
Kaciras <Kaciras@outlook.com>
2020-07-05 16:27:10 +0800

    (WIP) markdown-media.ts: remove properties stanx

--- packages/server/__tests__/markdown-media.spec.ts
+++ packages/server/__tests__/markdown-media.spec.ts
@@ -8,25 +8,11 @@ it("should convert to html", () => {
 	const markdown = `
 text before
 
-@gif[A gif video](/video/foo.mp4){ width="1920" height="1080" }
+@gif[A gif video](/video/foo.mp4)
 
 text after`
 
 	const html = markdownIt.render(markdown);
-	expect(html).toMatchSnapshot();
-});
-
-it("should unescape chars in label", () => {
-	const html = markdownIt.render('@video[/\\[hello\\]](/video/foo.mp4){loop width="1920" muted}');
-	expect(html).toMatchSnapshot();
-});
-
-it("should unescape chars in property", () => {
-	expect(markdownIt.render('@video[](/video/foo.mp4){ key="tes\\"t" }')).toMatchSnapshot();
-});
-
-it("should escape link", () => {
-	const html = markdownIt.render("@video[](javascript:xss){}");
 	expect(html).toMatchSnapshot();
 });
 
@@ -39,17 +25,9 @@ describe("tokenizer", () => {
 	}
 
 	it("should support escaping", () => {
-		markdownIt.render('@video[](){ key="\\\\foo_\\"bar\\"" }');
-		expect(token.properties.key).toBe('\\foo_"bar"');
-	});
-
-	it("should allow no char after key", () => {
-		markdownIt.render("@video[](){loop}");
-		expect(token.properties.loop).toBe(true);
-	});
-
-	it("should allow no char after value", () => {
-		markdownIt.render('@video[](){key="123"}');
-		expect(token.properties.key).toBe("123");
+		markdownIt.render("@gif[A gif video](/video/foo.mp4)");
+		expect(token.tag).toBe("gif");
+		expect(token.content).toBe("A gif video");
+		expect(token.src).toBe("/video/foo.mp4");
 	});
 });
--- packages/server/lib/markdown-media.ts
+++ packages/server/lib/markdown-media.ts
@@ -2,16 +2,13 @@
  * 自定义的Markdown语法，用于插入视频、音频等，
  * 该语句是一个块级元素，因为目前没有图文混排的需求。
  *
- * 格式：@<type>[<label>](<src>){ <properties> }
+ * 格式：@<type>[<label>](<src>)
  * - type: 类型
  * - label: 替代内容或标签
  * - src: 源链接
- * - properties: 附加属性
- *
- * 其中 { <properties> } 部分可用于设置宽高等，其应当是可省略的，Markdown的原则是只关注内容。
  *
  * Example:
- * @gif[A animated image](/data/gif-to-video.mp4){ width="300" height="100" }
+ * @gif[A animated image](/data/gif-to-video.mp4?vw=300&vh=100)
  *
  * 【为何不直接写HTML】
  * Markdown本身是跟渲染结果无关的，不应该和HTML绑死，而且写HTML不利于修改。
@@ -21,11 +18,8 @@ import MarkdownIt from "markdown-it";
 import Token from "markdown-it/lib/token";
 import StateBlock from "markdown-it/lib/rules_block/state_block";
 
-export type Properties = { [key: string]: string | true };
-
 export interface MediaToken extends Token {
 	src: string;
-	properties: Properties;
 }
 
 class RegexBuilder {
@@ -63,90 +57,21 @@ function parseMedia(state: StateBlock, startLine: number, endLine: number, silen
 		return false;
 	}
 
-	const [base, type, label, reference] = match;
+	const [, type, label, reference] = match;
 
+	// 谨防XSS
 	let link = md.normalizeLink(reference);
-	if (!md.validateLink(link)) {
-		link = ""; // 谨防XSS
-	}
-
-	const attributes: Properties = {};
-
-	// 有 { 需要解析属性段
-	if (src.charCodeAt(pos + base.length) === 0x7B) {
-		if (src.charCodeAt(posMax - 1) !== 0x7D) {
-			return false;
-		}
-		const section = src.slice(pos + base.length + 1, posMax - 1);
-		try {
-			parseProperties(section, attributes);
-		} catch (e) {
-			return false;
-		}
-	}
+	if (!md.validateLink(link)) link = "";
 
 	if (!silent) {
 		const token = state.push("media", type, 0) as MediaToken;
+		token.map = [startLine, state.line];
 		token.src = link;
 		token.content = state.md.utils.unescapeMd(label);
-		token.properties = attributes;
-		token.map = [startLine, state.line];
 	}
 
 	state.line = startLine + 1;
 	return true;
-}
-
-function unescape(value: string) {
-	return value.replace(/\\([\\"])/g, "$1")
-}
-
-const KEY_RE = /\s*(\w+)/;
-const VALUE_RE = /"(.*?)(?<!\\)"/;
-
-function isPairEnd(code: number) {
-	return Number.isNaN(code) || code === 0x20 || code === 0x09;
-}
-
-function parseProperties(src: string, attributes: Properties) {
-
-	function consumeToggle(kMatch: RegExpExecArray) {
-		attributes[kMatch[1]] = true;
-		src = src.slice(kMatch[0].length);
-	}
-
-	function consumeValue(kMatch: RegExpExecArray) {
-		const len = kMatch[0].length;
-		src = src.slice(len + 1);
-
-		const vMatch = VALUE_RE.exec(src);
-		if (!vMatch) {
-			throw new Error("Expect double quotes, but: " + src.charAt(len));
-		}
-
-		attributes[kMatch[1]] = unescape(vMatch[1]);
-		src = src.slice(vMatch[0].length);
-
-		if (!isPairEnd(src.charCodeAt(0))) {
-			throw new Error(`Invalid char: ${src.charAt(0)} after value`);
-		}
-	}
-
-	let kMatch = KEY_RE.exec(src);
-	while (kMatch) {
-		const len = kMatch[0].length;
-		const code = src.charCodeAt(len);
-
-		if (isPairEnd(code)) {
-			consumeToggle(kMatch);
-		} else if (code === 0x3D) {
-			consumeValue(kMatch);
-		} else {
-			throw new Error(`Invalid char: ${src.charAt(len)} after key`);
-		}
-
-		kMatch = KEY_RE.exec(src);
-	}
 }
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
@@ -155,19 +80,19 @@ function parseProperties(src: string, attributes: Properties) {
 
 function renderMedia(tokens: Token[], idx: number) {
 	const token = tokens[idx] as MediaToken;
-	const { tag, properties, src, content } = token;
+	const { tag, src, content } = token;
 
 	switch (tag) {
 		case "gif":
-			return renderGIFVideo(src, content, properties);
+			return renderGIFVideo(src, content);
 		case "video":
-			return renderVideo(src, content, properties);
+			return renderVideo(src, content);
 	}
 
 	throw new Error("Unsupported media type: " + tag);
 }
 
-function renderVideo(src: string, poster: string, _: Properties) {
+function renderVideo(src: string, poster: string) {
 	let attrs = `src="${src}"`;
 	if (poster) {
 		attrs += ` poster="${poster}"`;
@@ -175,7 +100,7 @@ function renderVideo(src: string, poster: string, _: Properties) {
 	return `<div class="video"><video ${attrs}></video></div>`;
 }
 
-function renderGIFVideo(src: string, _: string, __: Properties) {
+function renderGIFVideo(src: string, _: string) {
 	return `<div class="video"><video src="${src}" loop muted></video></div>`;
 }
 
