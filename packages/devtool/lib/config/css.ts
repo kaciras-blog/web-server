a99215ced5b662b58b97165067c45336d9a98f3f
Kaciras <Kaciras@outlook.com>
2020-07-26 17:13:06 +0800

    fix esModule

--- packages/devtool/lib/config/css.ts
+++ packages/devtool/lib/config/css.ts
@@ -60,6 +60,7 @@ export default function generateCssLoaders(options: LoaderChainOptions): RuleSet
 				// importLoaders指定了用CSS的 @import 语法导入文件时需要用 css-loader 前面的几个加载器处理。
 				// 在 css-loader 之前的有 postcss-loader 和一个可选的预处理器。
 				importLoaders: 1,
+				esModule: false,
 				sourceMap,
 				modules: modules && {
 					localIdentName: options.production
