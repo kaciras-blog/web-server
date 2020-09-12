20fb204c0231c070a36f6977a4b32e3e491829d4
Kaciras <Kaciras@outlook.com>
2020-09-12 19:58:21 +0800

    media: change hash name length to 20

--- packages/media/lib/common.ts
+++ packages/media/lib/common.ts
@@ -5,10 +5,8 @@ import log4js from "log4js";
 import { xxHash3_128 } from "@kaciras-blog/nativelib";
 
 /**
- * 对数据执行 Hash 运算，返回一个固定长度且唯一的字符串，可用于文件名。
- *
- * 该函数运算速度很快，暂时不用异步，详情见：
- * __perfs__\hash-mane.ts
+ * 对数据执行 Hash 运算，返回 20 个字符的 base64 字符串，可用于文件名。
+ * 该函数运算速度很快无需异步。
  *
  * 【Hash-Flooding Attack】
  * 非加密 Hash 有恶意碰撞的风险，用在哈希表上会导致退化从而产生性能问题。
@@ -17,20 +15,25 @@ import { xxHash3_128 } from "@kaciras-blog/nativelib";
  * 【文件系统的大小写敏感性】
  * base64 是大小写敏感的，但有些文件系统不敏感（HFS+, exFAT, etc...），
  * 使用大小写不敏感的文件系统会提升碰撞率。
- * 不过即便如此，等价的 Hash 位数也只是从 128 降低为 112，碰撞的可能性仍然微乎其微。
  *
- * 【为什么不用HEX】
- * 我有强迫症，能省几个字符坚决不用更长的，至于某些文件系统大小写不敏感那是它的事，
- * 只要 URL 是敏感的我这就要用！
+ * 在不敏感的系统上，base64 每个字符的种类由 64 降为 36，通过计算：
  *
- * 【末尾等号的问题】
- * 最后两个填充的等号看着很多余就删了，解码时记得把等号加回来。
+ *   log(2, pow(36, N)) / log(2, pow(64, N))
+ * = log(2, 6) / 3
+ * = 0.86165416690705206048457964798261
+ *
+ * 可以得出，在不敏感的系统上 base64 有效位数降低为原来的 0.86 倍。
+ *
+ * 20 位 base64 拥有 120 bit 信息，在不敏感的系统上降低为 103.4 bit，碰撞几率仍然很低。
+ *
+ * 【为什么不用 HEX】
+ * 我有强迫症，能省几个字符坚决不用更长的。
  *
  * @param buffer 数据
- * @return 22位字符串标识
+ * @return 字符串形式的 Hash 值
  */
 export function hashName(buffer: Buffer) {
-	return xxHash3_128(buffer, "base64u").slice(0, 22);
+	return xxHash3_128(buffer, "base64u").slice(0, 20);
 }
 
 /**
