7faa3bd5618c0aea2f849ae64dd19656a0f18ab9
Kaciras <Kaciras@outlook.com>
2020-09-23 14:53:21 +0800

    add a test for svgo

--- packages/image/__tests__/image-service-tests.ts
+++ packages/image/__tests__/image-service-tests.ts
@@ -1,3 +1,5 @@
+import path from "path";
+import fs from "fs-extra";
 import { PreGenerateImageService } from "../lib/image-service";
 import { runFilters } from "../lib/filter-runner";
 import { LocalFileSlot } from "../lib/image-store";
@@ -6,7 +8,10 @@ jest.mock("../lib/filter-runner");
 (runFilters as jest.Mock).mockImplementation((buffer) => Promise.resolve(buffer));
 
 const mockSlot = {
+	exists: jest.fn(),
+	save: jest.fn(() => Promise.resolve()),
 	getCache: jest.fn(),
+	putCache: jest.fn(),
 };
 
 function mockStore(ignore: string) {
@@ -30,5 +35,19 @@ describe("get", () => {
 
 		await service.get("test", "svg", { brotli: true });
 		expect(mockSlot.getCache.mock.calls).toHaveLength(2);
+	});
+});
+
+describe("save", () => {
+
+	// 怎么测试结果是无损的？
+	it("should optimize SVG", async () => {
+		const service = new PreGenerateImageService(mockStore as any);
+		const buffer = await fs.readFile(path.join(__dirname, "fixtures", "digraph.svg"));
+
+		mockSlot.exists.mockResolvedValue(false);
+		await service.save(buffer, "svg");
+
+		expect(mockSlot.putCache.mock.calls).toHaveLength(2);
 	});
 });
