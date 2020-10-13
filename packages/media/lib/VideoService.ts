d2ecbb02941bfc678586ce59f3ad48425efcae9b
Kaciras <Kaciras@outlook.com>
2020-10-13 21:40:35 +0800

    generify parameters

--- packages/media/lib/VideoService.ts
+++ packages/media/lib/VideoService.ts
@@ -4,8 +4,25 @@ import { BadDataError } from "./errors";
 import { LoadRequest, SaveRequest, WebFileService } from "./WebFileService";
 import { FileStore } from "./FileStore";
 
+interface SaveParams {
+
+	/**
+	 * 变体标识，为已上传视频的文件名，该参数表示新上传的视频将作已存在视频的变体，
+	 * 它将具有与其同样的访问 URL，在下载时智能选择。
+	 */
+	variant?: string;
+}
+
 const SUPPORTED_TYPES = ["mp4", "av1"];
 
+/**
+ * 【客户端转码】
+ * 由于视频转码很费时，放在服务端占用大量资源，所以暂未支持视频的优化。
+ * 这里选择让用户自己转码，然后上传同一视频的多个版本。
+ *
+ * 需要注意视频转码是有损的，这意味着难以检测上传的多个版本是否包含相同的内容，
+ * 如果上传了不同的视频作为变体，则不同的浏览器可能访问到不同的内容。
+ */
 export default class VideoService implements WebFileService {
 
 	private readonly store: FileStore;
@@ -22,15 +39,17 @@ export default class VideoService implements WebFileService {
 			.selectFirstMatch();
 	}
 
-	async save(request: SaveRequest) {
-		const { buffer, mimetype } = request;
-		const name = hashName(buffer);
+	async save(request: SaveRequest<SaveParams>) {
+		const { buffer, mimetype, parameters } = request;
 		const type = mime.extension(mimetype);
 
 		if (!type || SUPPORTED_TYPES.includes(type)) {
 			throw new BadDataError("不支持的视频格式：" + mimetype);
 		}
 
+		const name = parameters.variant ?? hashName(buffer);
 		await this.store.putCache(buffer, name, { type });
+
+		return { url: name };
 	}
 }
--- packages/media/lib/WebFileService.ts
+++ packages/media/lib/WebFileService.ts
@@ -4,24 +4,30 @@ export interface Params {
 	[key: string]: string;
 }
 
-export interface SaveRequest {
+export interface SaveRequest<T = Params> {
 	buffer: Buffer;
 	mimetype: string;
 	rawName: string;
-	parameters: Params;
+	parameters: T;
 }
 
-export interface LoadRequest {
+export interface LoadRequest<T = Params> {
 	name: string;
 	acceptTypes: string[];
 	acceptEncodings: string[];
-	parameters: Params;
+	parameters: T;
 }
 
+/**
+ * 保存请求的响应信息，这里只定义了必须的属性，实现时可以添加其他属性。
+ */
 export interface SaveResponse {
 	url: string;
 }
 
+/**
+ * 下载响应的信息，用于生成 HTTP 响应。
+ */
 export interface LoadResponse {
 	file: FileInfo;
 	mimetype: string;
