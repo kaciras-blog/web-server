150ca61aaf032d6b18d3fd7d1730dbeeb5fca801
Kaciras <Kaciras@protonmail.com>
2020-12-20 20:48:04 +0800

    devtool: remove StopWatch.ts in favor performance.now()

--- packages/devtool/lib/webpack/HooksInspectPlugin.ts
+++ packages/devtool/lib/webpack/HooksInspectPlugin.ts
@@ -1,8 +1,8 @@
+import { performance } from "perf_hooks";
 import chalk, { Chalk } from "chalk";
 import { Compiler, Plugin } from "webpack";
 import { Hook, HookMap } from "tapable";
 import { once } from "@kaciras-blog/server/lib/functions";
-import StopWatch from "../StopWatch";
 
 /**
  * 返回指定对象易读的类型字符串。
@@ -42,14 +42,15 @@ const COLOR_MAP: Readonly<{ [key: string]: Chalk }> = {
  */
 export default class HooksInspectPlugin implements Plugin {
 
-	private stopWatch = new StopWatch();
+	private startTime = 0;
+	private lastTime = 0;
 
 	apply(compiler: Compiler): void {
 		this.installHooks("Compiler", compiler);
 		this.setupTapable("NormalModuleFactory", compiler.hooks.normalModuleFactory);
 		this.setupTapable("ContextModuleFactory", compiler.hooks.contextModuleFactory);
 		this.setupTapable("Compilation", compiler.hooks.thisCompilation);
-		this.stopWatch.start();
+		this.lastTime = this.startTime = performance.now();
 	}
 
 	private setupTapable(namespace: string, tapable: Hook) {
@@ -82,8 +83,11 @@ export default class HooksInspectPlugin implements Plugin {
 	 */
 	private hookLogger(namespace: string, name: string) {
 		return once((...args: any[]) => {
+			const now = performance.now();
+			const duration = now - this.lastTime;
+			const time = now - this.startTime;
+
 			const argInfo = args.map(simpleTypeName).join(", ");
-			const [time, duration] = this.stopWatch.time();
 			const color = COLOR_MAP[namespace] || chalk.reset;
 
 			const timeLabel = `${time.toFixed(0)} [${duration.toFixed(3)}] - `;
