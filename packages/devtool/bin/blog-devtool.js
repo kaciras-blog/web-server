6821953d912354e5b20614f75fc5924434a16178
Kaciras <Kaciras@protonmail.com>
2021-03-03 15:48:28 +0800

    remove source-map-support in favor `--enable-source-maps` CLI argument

--- packages/devtool/bin/blog-devtool.js
+++ packages/devtool/bin/blog-devtool.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-require("source-map-support").install();
 const launcher = require("../lib/main").default;
 
 launcher.run(process.argv.slice(2));
--- packages/server/bin/blog-server.js
+++ packages/server/bin/blog-server.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-require("source-map-support").install();
 const Launcher = require("../lib/Launcher").default;
 
 new Launcher().run(process.argv.slice(2));
