36a9c67fdb38bc90a1ceae7f1c2a05ce0f50290e
Kaciras <Kaciras@protonmail.com>
2021-03-14 23:07:14 +0800

    Revert "remove source-map-support in favor `--enable-source-maps` CLI argument"
    This reverts commit 6821953d

--- packages/devtool/bin/blog-devtool.js
+++ packages/devtool/bin/blog-devtool.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+require("source-map-support").install();
 const launcher = require("../lib/main").default;
 
 launcher.run(process.argv.slice(2));
--- packages/server/bin/blog-server.js
+++ packages/server/bin/blog-server.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+require("source-map-support").install();
 const Launcher = require("../lib/Launcher").default;
 
 new Launcher().run(process.argv.slice(2));
