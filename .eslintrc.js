f5c6f46084b578dc54b4b86bb344d59f7b1041f9
Kaciras <Kaciras@protonmail.com>
2021-03-30 20:42:19 +0800

    use @kaciras/eslint-config-jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,14 +12,7 @@ module.exports = {
 	overrides: [
 		{
 			files: jestConfig.testMatch,
-			extends: [
-				"plugin:jest/style",
-				"plugin:jest/recommended",
-			],
-			rules: {
-				"jest/expect-expect": "off",
-				"jest/no-done-callback": "off",
-			},
+			extends: ["@kaciras/jest"],
 		},
 	],
 };
