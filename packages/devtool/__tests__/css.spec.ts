a349d6242d39be9eb72ac3beb217e8028ddead8f
Kaciras <Kaciras@protonmail.com>
2021-08-02 15:02:37 +0800

    fix css loader tests

--- packages/devtool/__tests__/css.spec.ts
+++ packages/devtool/__tests__/css.spec.ts
@@ -1,14 +1,21 @@
 import { resolveFixture, runWebpack } from "./test-utils";
 import generateCssLoaders from "../lib/config/css";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import path from "path";
 
 it("should support css imports", async () => {
 	const output = await runWebpack({
-		mode: "development",
+		mode: "production",
 		entry: resolveFixture("style.css"),
 		devtool: false,
 		output: {
 			path: "/",
+		},
+		resolveLoader: {
+			modules: [
+				"node_modules",
+				path.join(__dirname, "../node_modules"),
+			],
 		},
 		module: {
 			rules: generateCssLoaders({
@@ -27,11 +34,17 @@ it("should support css imports", async () => {
 
 it("should support less", async () => {
 	const output = await runWebpack({
-		mode: "development",
+		mode: "production",
 		entry: resolveFixture("less.less"),
 		devtool: false,
 		output: {
 			path: "/",
+		},
+		resolveLoader: {
+			modules: [
+				"node_modules",
+				path.join(__dirname, "../node_modules"),
+			],
 		},
 		module: {
 			rules: generateCssLoaders({
