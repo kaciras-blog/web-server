3c0cdd2b76a7e760098992e1dd2cde3d13181c51
Kaciras <Kaciras@protonmail.com>
2021-09-03 18:45:40 +0800

    remove dep `make-error`

--- packages/media/lib/errors.ts
+++ packages/media/lib/errors.ts
@@ -1,11 +1,8 @@
-// JS 这垃圾语言实现个自定义异常坑真是多，还得靠三方库……
-import { BaseError } from "make-error";
-
 /**
  * 表示用户输入的数据或参数错误。
  * 它们无法由程序处理，需要返回错误信息。
  */
-export abstract class MediaError extends BaseError {}
+export abstract class MediaError extends Error {}
 
 /**
  * 文件数据已损坏，或是输入了无法处理的数据。
@@ -30,4 +27,10 @@ export class ParamsError extends MediaError {}
 /**
  * 当出现一些无法继续处理的边界情况时抛出该异常，这些情况是可以预料的，应当在程序中处理。
  */
-export class ImageFilterException extends BaseError {}
+export class ImageFilterException extends Error {}
+
+// 设置 name 属性，因为似乎大多地方比起 instanceof 更喜欢用 name 判断异常类型。
+MediaError.prototype.name = "MediaError";
+BadDataError.prototype.name = "BadDataError";
+ParamsError.prototype.name = "ParamsError";
+ImageFilterException.prototype.name = "ImageFilterException";
