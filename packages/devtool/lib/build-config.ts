229d28f5283e415f47e84a1109ce1ef40dc0b2e3
Kaciras <Kaciras@protonmail.com>
2022-01-25 15:26:12 +0800

    add compress plugin for Vite

--- packages/devtool/lib/build-config.ts
+++ packages/devtool/lib/build-config.ts
@@ -1,8 +1,10 @@
 import { cwd } from "process";
 import path from "path";
 import { defineConfig } from "vite";
+import visualizer from "rollup-plugin-visualizer";
 import vue from "@vitejs/plugin-vue";
 import { ResolvedDevConfig } from "./options.js";
+import compress from "./plugin/compress.js";
 import vueSvgComponent from "./plugin/vue-svg-component.js";
 import optimizeImage from "./plugin/optimize-image.js";
 
@@ -31,10 +33,6 @@ export default function getViteConfig(options: ResolvedDevConfig) {
 					replacement: resolve("src/assets") + "/",
 				},
 			],
-			// alias: {
-			// 	"@/": "src/",
-			// 	"@assets/": "src/assets/",
-			// },
 		},
 		define: {
 			"process.env.API_ORIGIN": JSON.stringify(options.contentServer.publicOrigin),
@@ -48,15 +46,10 @@ export default function getViteConfig(options: ResolvedDevConfig) {
 
 			vueSvgComponent(),
 
-			// options.build.bundleAnalyzer && visualizer(),
+			options.build.bundleAnalyzer && visualizer(),
 
-			// isProd && compress({
-			// 	filter: /\.(js|css|html|svg)$/,
-			// }),
-			// isProd && compress({
-			// 	filter: /\.(js|css|html|svg)$/,
-			// 	algorithm: "brotliCompress",
-			// }),
+			compress({ algorithm: "gz" }),
+			compress({ algorithm: "br" }),
 
 			optimizeImage(new RegExp("static/")),
 		],
---
+++ packages/devtool/lib/plugin/compress.ts
@@ -0,0 +1,61 @@
+import { promisify } from "util";
+import { brotliCompress, gzip } from "zlib";
+import { OutputBundle } from "rollup";
+import { Plugin } from "vite";
+
+const zlibMap = {
+	gz: promisify(gzip),
+	br: promisify(brotliCompress),
+};
+
+export interface CompressOptions {
+	includes?: RegExp;
+	minSize?: number;
+	algorithm: keyof typeof zlibMap;
+}
+
+/**
+ * 压缩静态资源的插件，在打包的最后为输出的文件生成压缩的版本。
+ *
+ * <h2>为什么造轮子</h2>
+ * vite-plugin-compression 导出有问题，一个月没修。
+ * rollup-plugin-gzip API 反人类，压缩算法要传函数……
+ *
+ * 而且这种功能相当简单，实现起来不超过 100 行的，还是自己随手写一个吧。
+ *
+ * @param options 压缩选项
+ */
+export default function compressPlugin(options: CompressOptions): Plugin {
+	const {
+		algorithm,
+		minSize = 1024,
+		includes = /\.(m?js|json|svg|css|html)$/,
+	} = options;
+
+	const compress = zlibMap[algorithm];
+
+	return {
+		name: "kaciras:compress",
+		enforce: "post",
+
+		apply(config, env) {
+			return !config.build?.ssr && env.command === "build";
+		},
+
+		async generateBundle(_: unknown, bundle: OutputBundle) {
+			for (const [k, v] of Object.entries(bundle)) {
+				if (!includes.test(k)) {
+					continue;
+				}
+				let source = v.type === "chunk" ? v.code : v.source;
+
+				if (source.length < minSize) {
+					continue;
+				}
+				const fileName = `${v.fileName}.${algorithm}`;
+				source = await compress(source);
+				this.emitFile({ type: "asset", source, fileName });
+			}
+		},
+	};
+}
