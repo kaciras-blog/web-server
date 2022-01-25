955bd3c6cc681d50023106b8355b0410300c14e6
Kaciras <Kaciras@protonmail.com>
2022-01-25 20:02:24 +0800

    improve build logic

--- packages/devtool/lib/command/build.ts
+++ packages/devtool/lib/command/build.ts
@@ -1,12 +1,11 @@
 import getViteConfig from "../build-config.js";
 import { build } from "vite";
 import { cyan } from "colorette";
-import fs from "fs-extra";
 import { ResolvedDevConfig } from "../options";
 
-export default async function (options: ResolvedDevConfig) {
-	await fs.remove(options.outputDir);
+// Vite 会删除输出目录，无需自己再清理。
 
+export default async function (options: ResolvedDevConfig) {
 	const conf1 = getViteConfig(options) as any;
 	conf1.build.outDir = "dist/client";
 	await build(conf1);
@@ -14,6 +13,7 @@ export default async function (options: ResolvedDevConfig) {
 
 	const conf2 = getViteConfig(options) as any;
 	conf2.build.ssr = "src/entry-server.ts";
+	conf2.build.ssrManifest = true;
 	conf2.build.outDir = "dist/server";
 	await build(conf2);
 	console.log(cyan("Server build complete."));
