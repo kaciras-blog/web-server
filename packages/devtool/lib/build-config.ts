7e8635418ac88d57b555309a90f3f266a10461bd
Kaciras <Kaciras@protonmail.com>
2022-01-27 17:34:05 +0800

    remove redundant `@asset` alias

--- packages/devtool/lib/build-config.ts
+++ packages/devtool/lib/build-config.ts
@@ -1,5 +1,5 @@
 import { cwd } from "process";
-import path from "path";
+import { join } from "path";
 import { defineConfig } from "vite";
 import visualizer from "rollup-plugin-visualizer";
 import vue from "@vitejs/plugin-vue";
@@ -8,31 +8,13 @@ import compressAssets from "./plugin/compress-assets.js";
 import vueSvgComponent from "./plugin/vue-svg-component.js";
 import optimizeImage from "./plugin/optimize-image.js";
 
-/**
- * 将相对于 process.cwd 的路径转换为绝对路径。
- *
- * @param relativePath 相对路径
- * @return 对应的绝对路径
- */
-export function resolve(relativePath: string) {
-	return path.join(cwd(), relativePath);
-}
-
 export default function getViteConfig(options: ResolvedDevConfig) {
-	const isProd = options.build.mode === "production";
-
 	return defineConfig({
 		resolve: {
-			alias: [
-				{
-					find: new RegExp("@/"),
-					replacement: resolve("src") + "/",
-				},
-				{
-					find: new RegExp("@assets/"),
-					replacement: resolve("src/assets") + "/",
-				},
-			],
+			alias: [{
+				find: new RegExp("^@/"),
+				replacement: join(cwd(), "src") + "/",
+			}],
 		},
 		define: {
 			"process.env.API_ORIGIN": JSON.stringify(options.contentServer.publicOrigin),
