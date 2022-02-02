eef0f4bb6ede6c5b52d4af9ec74dd9be91be2e91
Kaciras <Kaciras@protonmail.com>
2022-02-02 14:05:10 +0800

    fix ssr tests

--- packages/server/__tests__/vue-ssr.spec.ts
+++ packages/server/__tests__/vue-ssr.spec.ts
@@ -1,47 +1,64 @@
 import { expect, it, vi } from "vitest";
 import supertest from "supertest";
 import Koa from "koa";
-import { RenderContext, renderPage } from "../lib/koa/vue-ssr";
+import { RenderContext, renderSSR } from "../lib/koa/vue-ssr";
 
-const renderFn = vi.fn<[RenderContext], Promise<string>>(() => Promise.resolve("test content"));
+const entry = vi.fn<[RenderContext]>(async () => "<p>bar</p>");
 
 const koa = new Koa();
-koa.use((ctx) => renderPage({ renderToString: renderFn } as any, ctx));
+koa.use(ctx => renderSSR(ctx, "<div>foo</div>", entry, {}));
 const callback = koa.callback();
 
-it("should render page", async () => {
+it("should pass the render context", async () => {
+	await supertest(callback).get("/test?a=b");
+
+	const [context] = entry.mock.calls[0];
+	expect(context.path).toBe("/test?a=b");
+	expect(context.status).toBeUndefined();
+	expect(context.error).toBeUndefined();
+	expect(context.template).toBe("<div>foo</div>");
+	expect(context.manifest).toStrictEqual({});
+});
+
+it("should render the page", async () => {
 	const request = supertest(callback).get("/test?a=b");
 	await request
 		.expect("Content-Type", /text\/html/)
-		.expect(200, "test content");
-
-	const context = renderFn.mock.calls[0][0];
-	expect(context.url.toString()).toBe(request.url);
+		.expect(200, "<p>bar</p>");
 });
 
-it("should respond with status 404 for nonexistent resource", async () => {
-	renderFn.mockImplementationOnce(async (ctx) => {
-		ctx.notFound = true;
-		return "not found";
+it("should support set status", async () => {
+	entry.mockImplementationOnce(async ctx => {
+		ctx.status = 418;
+		return "<span>baz</span>";
 	});
-	await supertest(callback).get("/test").expect(404);
+	await supertest(callback)
+		.get("/test")
+		.expect(418, "<span>baz</span>");
 });
 
 it("should perform custom redirect", async () => {
-	renderFn.mockRejectedValueOnce(Object.assign(new Error(), { code: 302, location: "/redirected" }));
+	entry.mockRejectedValueOnce(Object.assign(new Error(), { code: 302, location: "/redirected" }));
 	await supertest(callback).get("/test?a=b")
 		.expect(302)
 		.expect("Location", "/redirected");
 });
 
-it("should redirect to error page", async () => {
-	renderFn.mockRejectedValueOnce(new Error());
-	const request = supertest(callback).get("/test?a=b");
-	await request.expect(503);
+it("should render error page on error occurred", async () => {
+	const error = new Error();
+	entry.mockRejectedValueOnce(error);
+	entry.mockResolvedValueOnce("Test Error");
 
-	const context = renderFn.mock.calls[1][0];
-	expect(context.url.pathname).toBe("/error/500");
-	expect(context.url.search).toBe("");
+	await supertest(callback)
+		.get("/test?a=b")
+		.expect(503, "Test Error");
+
+	expect(entry.mock.calls).toHaveLength(2);
+
+	const [context] = entry.mock.calls[1];
+	expect(context.path).toBe("/test?a=b");
+	expect(context.error).toBe(error);
+	expect(context.status).toBeUndefined();
 });
 
 // createSSRProductionPlugin() 不测了，就是一个便捷的封装方法。
