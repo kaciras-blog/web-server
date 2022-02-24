01a36fac340d3f49f8241685a76e9a0c569ef54a
Kaciras <Kaciras@protonmail.com>
2022-02-24 15:38:03 +0800

    update deps

--- packages/devtool/lib/index.ts
+++ packages/devtool/lib/index.ts
@@ -3,7 +3,6 @@ export { default as getViteConfig } from "./build-config.js";
 export { default as serve } from "./command/serve.js";
 export { default as build } from "./command/build.js";
 
-export { default as vueSvgComponent } from "./plugin/vue-svg-component.js";
 export { default as compressAssets } from "./plugin/compress-assets.js";
 export { default as processImage } from "./plugin/process-image.js";
 export { default as optimizeImage } from "./plugin/optimize-image.js";
