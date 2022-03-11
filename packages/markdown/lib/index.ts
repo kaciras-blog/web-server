533e92fe3ab8672bcf7e11bfdbee083b0f2b6699
Kaciras <Kaciras@protonmail.com>
2022-03-11 18:15:53 +0800

    markdown: add anchor & TOC plugin

--- packages/markdown/lib/index.ts
+++ packages/markdown/lib/index.ts
@@ -1,4 +1,23 @@
+import MarkdownIt from "markdown-it";
+import AnchorRaw from "markdown-it-anchor";
+
+export { default as TOC } from "markdown-it-toc-done-right";
+
 export * from "./media.js";
 
 export { default as UGC } from "./ugc.js";
 export { default as Media } from "./media.js";
+
+export function Anchor(markdownIt: MarkdownIt) {
+	markdownIt.use<AnchorRaw.AnchorOptions>(Anchor, {
+
+		// 参考 MSDN 网站的做法，有 aria-labelledby 情况下不再需要内容
+		permalink: AnchorRaw.permalink.linkInsideHeader({
+			placement: "after",
+			ariaHidden: true,
+			class: "anchor-link",
+		}),
+
+		slugify: title => title.trim().toLowerCase().replace(/\s+/g, "-"),
+	});
+}
