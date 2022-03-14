590a8979cf8c006f060352a668a5b8ed574a845f
Kaciras <Kaciras@protonmail.com>
2022-03-14 14:24:30 +0800

    oops

--- packages/markdown/lib/index.ts
+++ packages/markdown/lib/index.ts
@@ -9,7 +9,7 @@ export { default as UGC } from "./ugc.js";
 export { default as Media } from "./media.js";
 
 export function Anchor(markdownIt: MarkdownIt) {
-	markdownIt.use<AnchorRaw.AnchorOptions>(Anchor, {
+	markdownIt.use<AnchorRaw.AnchorOptions>(AnchorRaw, {
 
 		// 参考 MSDN 网站的做法，有 aria-labelledby 情况下不再需要内容
 		permalink: AnchorRaw.permalink.linkInsideHeader({
