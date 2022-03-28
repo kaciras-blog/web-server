d155b4721597b29d267b9897d371b960a3dc3851
Kaciras <Kaciras@protonmail.com>
2022-03-28 20:21:09 +0800

    add COOP header

--- packages/server/lib/blog.ts
+++ packages/server/lib/blog.ts
@@ -31,19 +31,28 @@ const CSP_REPORT_URI = "/csp-report";
  * <h2>Content-Security-Policy</h2>
  * frame-ancestors: 没想出有什么嵌入其它网站的必要。
  * object-src: 禁止一些过时元素。
- * block-all-mixed-content: 我全站都是HTTPS，也不太会去用HTTP的服务。
+ * block-all-mixed-content: 我全站都是 HTTPS，也不太会去用 HTTP 的服务。
  * 其他的限制太死了，暂时没开启。
+ *
+ * <h2>COEP & COOP</h2>
+ * COEP 图片视频等资源只能从同源或者支持 CORS 的三方加载，禁止传统的非 CORS 模式。
+ *      如果要用第三方图床、视频之类的需要注意。
+ * COOP 阻止了跨域跳转和弹窗时的 window.opener 访问，一些老旧的支付系统可能用到，本站没有。
+ *
+ * 这两个头部同时使用可以解锁 SharedArrayBuffer 等功能。
  *
  * <h2>HSTS</h2>
  * 纯粹是跟底层 TLS 相关的头，不应该在这里配置，而且在 localhost 下会弹一堆警告。
  */
 async function harden(ctx: BaseContext, next: Next) {
 	await next();
+	// ctx.set("Cross-Origin-Embedder-Policy", "require-corp");
+	ctx.set("Cross-Origin-Opener-Policy", "same-origin");
 	ctx.set("Content-Security-Policy", "frame-ancestors 'self'; " +
 		"object-src 'none'; block-all-mixed-content; report-uri " + CSP_REPORT_URI);
 }
 
-function cspReporter(ctx: ExtendableContext) {
+function reportCSP(ctx: ExtendableContext) {
 	ctx.status = 204;
 	if (ctx.request.body) {
 		logger.warn("CSP Violation: ", ctx.request.body);
@@ -100,7 +109,7 @@ export default function getBlogPlugin(options: ResolvedConfig): FunctionPlugin {
 		router.get("/feed/:type", feedHandler(options));
 		router.get("/sw-check", toggleSW(app.serviceWorker));
 		router.get("/sitemap.xml", sitemapHandler(address));
-		router.post(CSP_REPORT_URI, cspReporter);
+		router.post(CSP_REPORT_URI, reportCSP);
 
 		// 过大的媒体建议直接传到第三方存储
 		const multerInstance = multer({ limits: { fileSize: 50 * 1024 * 1024 } });
