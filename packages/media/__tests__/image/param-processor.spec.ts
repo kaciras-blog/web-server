4cd2b7b26e73efec2cf670fec1285aa1cdb3b189
Kaciras <Kaciras@protonmail.com>
2022-04-29 17:08:49 +0800

    add flip & rotate support to param-processor

--- packages/media/__tests__/image/param-processor.spec.ts
+++ packages/media/__tests__/image/param-processor.spec.ts
@@ -1,23 +1,76 @@
-import { expect, it } from "vitest";
+import { describe, expect, it } from "vitest";
 import sharp from "sharp";
-import { crop, ParamsError, resize } from "../../lib";
+import { crop, flip, ParamsError, resize, rotate } from "../../lib/index";
 import { readFixture } from "../test-utils";
 
 const buffer = readFixture("tile_16x16.png");
 
-it("should throw on invalid crop parameter", () => {
-	expect(() => crop(sharp(), "_INVALID_")).toThrow(ParamsError);
+describe("crop", () => {
+	it.each([
+		"_INVALID_",
+		"",
+		"a-b-c-d",
+	])("should throw on invalid parameter %#", (arg) => {
+		expect(() => crop(sharp(), arg)).toThrow(ParamsError);
+	});
+
+	it("should crop the image", async () => {
+		const cropped = crop(sharp(buffer), "0-0-8-8");
+		const pixels = await cropped.raw().toBuffer();
+
+		// PNG 的压缩编码可能因依赖升级而变动，故此处采用为像素验证
+		expect(pixels).toHaveLength(256); // 8(width) x 8(height) x 4(RGBA)
+		pixels.forEach(bit8 => expect(bit8).toBe(0));
+	});
 });
 
-it("should throw on invalid resize parameter", () => {
-	expect(() => resize(sharp(), "_INVALID_")).toThrow(ParamsError);
+describe("flip", () => {
+	it.each([
+		"_INVALID_",
+		"",
+		"YY",
+		"XYX",
+	])("should throw on invalid parameter %#", (arg) => {
+		expect(() => flip(sharp(), arg)).toThrow(ParamsError);
+	});
+
+	it("should flip the image", async () => {
+		const cropped = flip(sharp(buffer), "X");
+		const raw = await cropped.raw().toBuffer();
+
+		const pixels = new Uint32Array(raw.buffer);
+		expect(pixels[0]).toBe(0xFF000000);
+		expect(pixels[15]).toBe(0);
+		expect(pixels[240]).toBe(0);
+		expect(pixels[255]).toBe(0xFFFFFFFF);
+	});
 });
 
-it("should crop the image", async () => {
-	// PNG 的压缩编码可能因依赖升级而变动，故此处转换为像素验证
-	const cropped = await crop(sharp(buffer), "0-0-8-8");
-	const pixels = await cropped.raw().toBuffer();
+describe("rotate", () => {
+	it.each([
+		"_INVALID_",
+		"",
+	])("should throw on invalid parameter %#", (arg) => {
+		expect(() => rotate(sharp(), arg)).toThrow(ParamsError);
+	});
 
-	expect(pixels).toHaveLength(256); // 8(width) x 8(height) x 4(RGBA)
-	pixels.forEach(bit8 => expect(bit8).toBe(0));
+	it("should rotate the image", async () => {
+		const cropped = rotate(sharp(buffer), "90");
+		const raw = await cropped.raw().toBuffer();
+
+		const pixels = new Uint32Array(raw.buffer);
+		expect(pixels[0]).toBe(0xFFFFFFFF);
+		expect(pixels[15]).toBe(0);
+		expect(pixels[240]).toBe(0);
+		expect(pixels[255]).toBe(0xFF000000);
+	});
+});
+
+describe("resize", () => {
+	it.each([
+		"_INVALID_",
+		"",
+	])("should throw on invalid parameter %#", (arg) => {
+		expect(() => resize(sharp(), arg)).toThrow(ParamsError);
+	});
 });
--- packages/media/lib/image/param-processor.ts
+++ packages/media/lib/image/param-processor.ts
@@ -23,6 +23,41 @@ export function crop(image: Sharp, argument: string) {
 }
 
 /**
+ * 翻转图片，参数为 XY 两个字母，顺序任意，也可以仅有其中一个。
+ *
+ * 两个字母分别表示以该轴翻转，例如：
+ * - flip=X：		  翻转 X 轴（上下颠倒）。
+ * - flip=Y：		  翻转 Y 轴（左右颠倒）。
+ * - flip=XY（或 YX）：两个轴都翻转。
+ */
+export function flip(image: Sharp, argument: string) {
+	switch (argument) {
+		case "X":
+			return image.flop();
+		case "Y":
+			return image.flip();
+		case "XY":
+		case "YX":
+			return image.flop().flip();
+	}
+	throw new ParamsError("翻转参数错误：" + argument);
+}
+
+/**
+ * 旋转图片，目前仅支持 90 度的倍数，参数格式 <deg> 单位角度。
+ * 注意调用顺序，先旋转后裁剪和先裁剪后选择是不一样的。
+ *
+ * @see https://sharp.pixelplumbing.com/api-operation#rotate
+ */
+export function rotate(image: Sharp, argument: string) {
+	const deg = parseInt(argument);
+	if (Number.isInteger(deg)) {
+		return image.rotate(deg);
+	}
+	throw new ParamsError("旋转参数错误：" + argument);
+}
+
+/**
  * 根据参数缩放图片，参数格式：<宽>x<高>，单位像素，表示把指定的方向缩放到该大小。
  * 宽和高都可以省略，表示该方向上不改变。
  *
