4c231a96a9e1f8871709bf2a604d6c4a2db43210
Kaciras <Kaciras@protonmail.com>
2022-06-04 22:45:02 +0800

    test optimize-image plugin with mocked optimizers

--- packages/devtool/__tests__/optimize-image.spec.ts
+++ packages/devtool/__tests__/optimize-image.spec.ts
@@ -1,8 +1,12 @@
-import { statSync } from "fs";
-import { expect, it } from "vitest";
+import { readFileSync, statSync } from "fs";
+import { expect, it, vi } from "vitest";
 import { RollupOutput } from "rollup";
+import { RasterOptimizer, SVGOptimizer } from "@kaciras-blog/media";
 import { avoidEmptyChunkTS, getAsset, resolveFixture, runVite } from "./test-utils";
 import optimizeImage from "../lib/plugin/optimize-image";
+
+const buildRaster = vi.spyOn(RasterOptimizer.prototype, "buildCache");
+const buildSVG = vi.spyOn(SVGOptimizer.prototype, "buildCache");
 
 /**
  * 断言指定名称的图片已输出，但未被优化。
@@ -10,21 +14,9 @@ import optimizeImage from "../lib/plugin/optimize-image";
  * @param bundle 构建结果
  * @param name 文件名
  */
-async function expectUnoptimized(bundle: RollupOutput, name: string) {
+function expectUnoptimized(bundle: RollupOutput, name: string) {
 	const source = statSync(resolveFixture(name));
 	expect(getAsset(bundle, name).length).toStrictEqual(source.size);
-}
-
-/**
- * 断言指定名称的图片已输出，且已被优化，大小小于原始的文件。
- *
- * @param bundle 构建结果
- * @param name 输出文件名
- * @param srcName 原始文件名，没有则与输出名相同
- */
-async function expectSmaller(bundle: RollupOutput, name: string, srcName = name) {
-	const source = statSync(resolveFixture(srcName));
-	expect(getAsset(bundle, name).length).toBeLessThan(source.size);
 }
 
 function optimize(input: string, include?: RegExp) {
@@ -40,27 +32,65 @@ function optimize(input: string, include?: RegExp) {
 }
 
 it("should compress images", async () => {
-	const bundle = await optimize("test.png");
+	buildRaster.mockResolvedValue(
+		[{ data: Buffer.from("111"), params: { type: "foo" } }],
+	);
 
-	// 1 个图片输出，1 个额外的 WebP，1 个额外的 AVIF，1 个入口文件。
-	expect(bundle.output).toHaveLength(4);
-	await expectSmaller(bundle, "test.png");
-	await expectSmaller(bundle, "test.webp", "test.png");
-	await expectSmaller(bundle, "test.avif", "test.png");
+	await optimize("test.png");
+
+	expect(buildRaster).toHaveBeenCalledTimes(1);
+
+	const [info] = buildRaster.mock.calls[0];
+	expect(info.type).toBe("png");
+	expect(info.parameters).toStrictEqual({});
+	expect(info.buffer).toStrictEqual(readFileSync(resolveFixture("test.png")));
 });
 
-it("should optimize SVG", async () => {
+it("should emit additional files", async () => {
+	buildRaster.mockResolvedValue(
+		[{ data: Buffer.from("111"), params: { type: "foo" } }],
+	);
+
+	const bundle = await optimize("test.png");
+
+	// 1 个优化后的，1 个原始图片，1 个入口文件。
+	expect(bundle.output).toHaveLength(3);
+	expectUnoptimized(bundle, "test.png");
+	expect(getAsset(bundle, "test.foo")).toStrictEqual(Buffer.from("111"));
+});
+
+it("should override original if needed", async () => {
+	buildRaster.mockResolvedValue(
+		[{ data: Buffer.from("111"), params: { type: "png" } }],
+	);
+
+	const bundle = await optimize("test.png");
+
+	// 1 个优化后的，1 个入口文件。
+	expect(bundle.output).toHaveLength(2);
+	expect(getAsset(bundle, "test.png")).toStrictEqual(Buffer.from("111"));
+});
+
+it("should append encoding suffix", async () => {
+	buildSVG.mockResolvedValue(
+		[{ data: Buffer.from("111"), params: { type: "svg", encoding: "gzip" } }],
+	);
+
 	const bundle = await optimize("visible-off.svg");
 
-	// 1 个图片输出，1 个入口文件。
-	expect(bundle.output).toHaveLength(2);
-	await expectSmaller(bundle, "visible-off.svg");
+	// 1 个优化后的，1 个原始图片，1 个入口文件。
+	expect(bundle.output).toHaveLength(3);
+	expect(getAsset(bundle, "visible-off.svg.gz")).toStrictEqual(Buffer.from("111"));
 });
 
 it("should include files matches the regexp", async () => {
+	buildRaster.mockResolvedValue(
+		[{ data: Buffer.from("111"), params: { type: "gif" } }],
+	);
+
 	const bundle = await optimize("entry-images.js", /\.gif$/);
 
-	await expectSmaller(bundle, "tantrum.gif");
-	await expectUnoptimized(bundle, "visible-off.svg");
-	await expectUnoptimized(bundle, "test.png");
+	expectUnoptimized(bundle, "visible-off.svg");
+	expectUnoptimized(bundle, "test.png");
+	expect(getAsset(bundle, "tantrum.gif")).toStrictEqual(Buffer.from("111"));
 });
