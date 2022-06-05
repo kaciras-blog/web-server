d47f5e980a1447139811e29afabb100f6b47973f
Kaciras <Kaciras@protonmail.com>
2022-06-05 14:59:49 +0800

    more tests for encoder.ts

--- packages/media/__tests__/image/encoder.spec.ts
+++ packages/media/__tests__/image/encoder.spec.ts
@@ -17,29 +17,70 @@ describe("For non-image data", () => {
 		return expect(optimizeRaster(buffer, type)).rejects.toBeInstanceOf(BadDataError);
 	}
 
-	it("should throw jpg", () => testFor("jpg"));
-	it("should throw png", () => testFor("png"));
-	it("should throw gif", () => testFor("gif"));
+	it("should fail when encode to jpg", () => testFor("jpg"));
+	it("should fail when encode to png", () => testFor("png"));
+	it("should fail when encode to gif", () => testFor("gif"));
+
+	it("should fail when encode to WebP", () => {
+		expect(encodeWebp(buffer)).rejects.toBeInstanceOf(BadDataError);
+	});
+
+	it("should fail when encode to AVIF", () => {
+		expect(encodeAVIF(buffer)).rejects.toBeInstanceOf(BadDataError);
+	});
 });
 
 // 对于损坏的图片数据，应当抛出 MediaError 异常
 describe("For bad image", () => {
+	const badPng = readFixture("bad_image.png");
 
 	async function testFor(srcType: string, targetType: string) {
 		const buffer = readFixture("bad_image." + srcType);
 		await expect(optimizeRaster(buffer, targetType)).rejects.toBeInstanceOf(BadDataError);
 	}
 
-	it("should throws on optimize jpg", () => testFor("jpg", "jpg"));
-	it("should throws on optimize png", () => testFor("png", "png"));
-	it("should throws on optimize gif", () => testFor("gif", "gif"));
+	it("should fail with bad jpg", () => testFor("jpg", "jpg"));
+	it("should fail with bad png", () => testFor("png", "png"));
+	it("should fail with bad gif", () => testFor("gif", "gif"));
+
+	it("should fail when encode to WebP", () => {
+		expect(encodeWebp(badPng)).rejects.toBeInstanceOf(BadDataError);
+	});
+
+	it("should fail when encode to AVIF", () => {
+		expect(encodeAVIF(badPng)).rejects.toBeInstanceOf(BadDataError);
+	});
 });
 
 describe("optimization", () => {
-	const colorText = readFixture("color_text_black_bg.png");
-	const gif = readFixture("animated.gif");
+	const gif = readFixture("test.gif");
+	const jpg = readFixture("test.jpg");
+	const png = readFixture("test.png");
 
-	// 这张图片如果用默认的参数转换为 webp 反而会变大，且失真严重
+	const colorText = readFixture("color_text_black_bg.png");
+
+	it("should optimize jpg files", async () => {
+		const result = await optimizeRaster(jpg, "jpg");
+
+		expect((await fileTypeFromBuffer(result))?.mime).toBe("image/jpeg");
+		expect(result.length).toBeLessThan(jpg.length);
+	});
+
+	it("should optimize gif files", async () => {
+		const result = await optimizeRaster(gif, "gif");
+
+		expect((await fileTypeFromBuffer(result))?.mime).toBe("image/gif");
+		expect(result.length).toBeLessThan(gif.length);
+	});
+
+	it("should optimize png files", async () => {
+		const result = await optimizeRaster(png, "png");
+
+		expect((await fileTypeFromBuffer(result))?.mime).toBe("image/png");
+		expect(result.length).toBeLessThan(png.length);
+	});
+
+	// 这张图片如果用默认的参数转换为 WebP 反而会变大，且失真严重
 	it("should effect on particular image", async () => {
 		const result = await encodeWebp(colorText);
 
@@ -47,7 +88,7 @@ describe("optimization", () => {
 		expect(result.length).toBeLessThan(colorText.length / 2);
 	});
 
-	it("should convert image to AVIF", async () => {
+	it("should convert the image to AVIF", async () => {
 		const result = await encodeAVIF(colorText);
 
 		expect((await fileTypeFromBuffer(result))?.mime).toBe("image/avif");
@@ -69,7 +110,7 @@ describe("optimization", () => {
 		expect(result.length).toBeLessThan(gif.length * 0.7);
 	});
 
-	// GIF 转 AVIF 效果也理想。
+	// GIF 转 AVIF 效果也不理想。
 	it("should not support GIF to WebP", () => {
 		return expect(encodeAVIF(gif)).rejects.toThrow(ProcessorError);
 	});
