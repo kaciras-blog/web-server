c865e15bce4b2d095c8ec5b84c4abdc282564157
Kaciras <Kaciras@protonmail.com>
2022-08-14 17:21:46 +0800

    remove unused process-image plugin

--- packages/devtool/lib/index.ts
+++ packages/devtool/lib/index.ts
@@ -4,5 +4,4 @@ export { default as serve } from "./command/serve.js";
 export { default as build } from "./command/build.js";
 
 export { default as compressAssets } from "./plugin/compress-assets.js";
-export { default as processImage } from "./plugin/process-image.js";
 export { default as optimizeImage } from "./plugin/optimize-image.js";
