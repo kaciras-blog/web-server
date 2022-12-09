d863fb3e80f55e52e0b6fa558d6942082afe60f7
Kaciras <Kaciras@protonmail.com>
2022-12-09 13:32:26 +0800

    remove source-map-support in favor `--enable-source-maps`

--- packages/devtool/bin/blog-devtool.ts
+++ packages/devtool/bin/blog-devtool.ts
@@ -1,8 +1,5 @@
 #!/usr/bin/env node
 import { argv } from "node:process";
-import { install } from "source-map-support";
 import launcher from "../lib/main.js";
-
-install();
 
 await launcher.run(argv.slice(2));
--- packages/server/bin/blog-server.ts
+++ packages/server/bin/blog-server.ts
@@ -1,8 +1,5 @@
 #!/usr/bin/env node
 import { argv } from "node:process";
-import { install } from "source-map-support";
 import { Launcher } from "../lib/index.js";
-
-install();
 
 await new Launcher().run(argv.slice(2));
