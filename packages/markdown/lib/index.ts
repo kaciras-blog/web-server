9e5ddc02bda92dc998e5da76210a41cb08da2a1e
Kaciras <Kaciras@protonmail.com>
2023-02-22 21:29:11 +0800

    markdown: introduce CollectLinks plugin

--- packages/markdown/lib/index.ts
+++ packages/markdown/lib/index.ts
@@ -1,3 +1,4 @@
+import Token from "markdown-it/lib/token.js";
 import MarkdownIt from "markdown-it";
 import FootnoteRaw from "markdown-it-footnote";
 import AnchorRaw from "markdown-it-anchor";
@@ -54,4 +55,32 @@ export function Classify(markdownIt: MarkdownIt) {
 		token.attrPush(["class", "inline-code"]);
 		return raw(tokens, idx, options, env, self);
 	};
+}
+
+/**
+ * 检查文本中所有文件链接的插件，文件链接包括链接的 href，以及媒体的源。
+ *
+ * @param markdownIt 要安装到的 MarkdownIt 对象。
+ * @param handler 找到的链接将传递给这个函数。
+ */
+export function CollectLinks(markdownIt: MarkdownIt, handler: (url: string) => void) {
+
+	function check(tokens: Token[]) {
+		for (const token of tokens) {
+			switch (token.type) {
+				case "inline":
+					check(token.children!);
+					break;
+				case "link_open":
+				case "media":
+					handler(token.attrGet("href")!);
+					break;
+				case "image":
+					handler(token.attrGet("src")!);
+					break;
+			}
+		}
+	}
+
+	markdownIt.core.ruler.push("collect-links", s => check(s.tokens));
 }
